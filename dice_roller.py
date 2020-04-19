b4a606e653893acad6ac0990a409c41672482034
alanta335 <55522447+alanta335@users.noreply.github.com>
2020-04-20 00:17:11 +0500

    add number of players

--- dice_roller.py
+++ dice_roller.py
@@ -3,16 +3,23 @@ def main():
   dice_rolls = int(input('How many dice would you like to roll? '))
   dice_sum = 0
   dice_size = int(input('How many sides are the dice? '))
-  for i in range(0,dice_rolls):
-    roll = random.randint(1,dice_size)
-    dice_sum += roll
-    if roll == 1:
-      print(f'You rolled a {roll}! Critical Fail')
-    elif roll == dice_size:
-      print(f'You rolled a {roll}! Critical Success!')
+  player = int(input('how many players are there? '))
+  for x in range(0,player):
+    print(f'player { x+1 }\'s press s to roll')
+    y = str(input())
+    if y == 's': 
+      for i in range(0,dice_rolls):
+        roll = random.randint(1,dice_size)
+        dice_sum += roll
+        if roll == 1:
+          print(f'You rolled a {roll}! Critical Fail')
+        elif roll == dice_size:
+          print(f'You rolled a {roll}! Critical Success!')
+        else:
+          print(f'You rolled a {roll}')
+      print(f'You have rolled a total of {dice_sum}')
     else:
-      print(f'You rolled a {roll}')
-  print(f'You have rolled a total of {dice_sum}')
+      print('skiped your turn')
   
 if __name__== "__main__":
   main()
