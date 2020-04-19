3ff062d34f93e78b55e7be52cbcffa1fbd96fcf5
alanta335 <55522447+alanta335@users.noreply.github.com>
2020-04-20 00:37:29 +0500

    player totoal score are stored in an array

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,7 @@
 import random
 def main():
+  dice_sum = [0 for x in range(50)]
   dice_rolls = int(input('How many dice would you like to roll? '))
-  dice_sum = 0
   dice_size = int(input('How many sides are the dice? '))
   player = int(input('how many players are there? '))
   for x in range(0,player):
@@ -10,14 +10,14 @@ def main():
     if y == 's': 
       for i in range(0,dice_rolls):
         roll = random.randint(1,dice_size)
-        dice_sum += roll
+        dice_sum[x] += roll
         if roll == 1:
           print(f'You rolled a {roll}! Critical Fail')
         elif roll == dice_size:
           print(f'You rolled a {roll}! Critical Success!')
         else:
           print(f'You rolled a {roll}')
-      print(f'You have rolled a total of {dice_sum}')
+      print(f'player {x+1} rolled a total of {dice_sum[x]}')
     else:
       print('skiped your turn')
   
